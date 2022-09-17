05e48e7c2473d2cb3a3d994e6fbedc972cb6f9e4
monish <samuelmonish1@gmail.com>
2022-09-17 12:47:15 +0530

    added

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import Alphabet
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/calc', methods=['POST'])
+def run_code():
+    string = request.form['string']
+    char = request.form['character']
+    leng = Alphabet.word_length(string)
+    matching = Alphabet.matching_char(string, char)
+    palin = Alphabet.palindrome_or_not(string)
+    rever = Alphabet.reverse(string)
+    return render_template('pass.html', l=leng, m=matching, p=palin, r=rever)
+
